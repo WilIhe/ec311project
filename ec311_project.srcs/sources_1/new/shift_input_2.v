5a596bd21b24d359a09a7a3afd8e70d0ba7eba1c
bperbu <bperales@bu.edu>
2026-04-23 03:21:31 -0400

    Update shift_input_2.v
    edited for user choice of a message or char to morse code

--- ec311_project.srcs/sources_1/new/shift_input_2.v
+++ ec311_project.srcs/sources_1/new/shift_input_2.v
@@ -1,1 +1,83 @@
-`timescale 1ns/1ps
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/18/2026 05:59:03 PM
+// Design Name: 
+// Module Name: shift_input
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module shift_input(
+    input [6:0] encode,
+    input clk, enable, rst,
+    input start,
+    output reg done_char,
+    output reg busy,
+    output reg morse_shift
+    );
+    
+    // This module acts like a shift register; it takes in the 20 bit morse sequence,
+    // and outputs it one at a time
+     
+    wire [19:0] morse_sequence;
+    wire slowed_clk;
+     
+    clk_div C2 (.clk_in(clk), .rst(rst), .divided_clk(slowed_clk));
+    morse_lookup ML1 (.encode(encode), .enable(enable), .clk(clk), .morse_sequence(morse_sequence));
+ 
+    
+    reg [4:0] index;
+
+    initial begin
+      index = 5'd19;
+      morse_shift = 0;
+      busy = 0;
+      done_char = 0;
+    end
+
+    always @ (posedge slowed_clk, posedge rst) begin 
+        if (rst) begin // re-outputs the morse code sequence
+            index <= 5'd19;
+            morse_shift <= 0;
+            busy <= 0;
+            done_char <= 0;
+        end
+        else begin
+          done_char <= 0;
+          if (start && !busy) begin
+            index <= 5'd19;
+            morse_shift <= morse_sequence[5'd19];
+            busy <= 1;
+          end
+          else if (busy) begin
+            morse_shift <= morse_sequence[index];
+            
+            if (index == 5'd0) begin 
+              index <= 5'd19;
+              busy <= 0;
+              done_char <= 1;
+              index <= 5'd19;
+            end
+            else begin 
+              index = index - 5'd1;
+            end
+          end
+          else begin
+                morse_shift <= 0;
+          end
+       end  
+    end
+endmodule
